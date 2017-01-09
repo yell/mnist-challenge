22a12472c756898eb75944c059f3add2d97f13c8
monsta <yell.bondarenko@gmail.com>
2017-01-09 18:15:35 +0100

    added + tested accuracy score

--- ml1_mnist/metrics/_metrics.py
+++ ml1_mnist/metrics/_metrics.py
@@ -1,9 +1,37 @@
+# TODO: add validation routine (numpyify if needed + check equal lengths)
 import numpy as np
 
 
-def zero_one_loss(y_true, y_pred, normalize=True):
+def accuracy_score(y_true, y_pred, normalize=True):
+    """Accuracy classification score.
+
+    Parameters
+    ----------
+    y_true : (n_samples, n_outputs) array-like
+        Ground truth (correct) labels.
+    y_pred : (n_samples, n_outputs) array-like
+        Predicted labels, as returned by a classifier.
+    normalize : bool, optional
+        If False, return the number of correctly classified samples.
+        Otherwise, return the fraction of correctly classified samples.
+
+    Returns
+    -------
+    score : float or int
+        If `normalize` == True, return the correctly classified
+        samples (float), else return the number of correctly
+        classified samples (int).
     """
-    Zero-one classification loss.
+    if not isinstance(y_true, np.ndarray): y_true = np.array(y_true)
+    if not isinstance(y_pred, np.ndarray): y_pred = np.array(y_pred)
+    score = sum(np.all(z == t) for z, t in zip(y_pred, y_true))
+    if normalize:
+        score /= float(len(y_true))
+    return score
+
+
+def zero_one_loss(y_true, y_pred, normalize=True):
+    """Zero-one classification loss.
 
     Parameters
     ----------
@@ -22,13 +50,11 @@ def zero_one_loss(y_true, y_pred, normalize=True):
         misclassifications (float), else it returns
         the number of misclassifications (int).
     """
-    if not isinstance(y_true, np.ndarray):
-        y_true = np.array(y_true)
-    if not isinstance(y_pred, np.ndarray):
-        y_pred = np.array(y_pred)
+    if not isinstance(y_true, np.ndarray): y_true = np.array(y_true)
+    if not isinstance(y_pred, np.ndarray): y_pred = np.array(y_pred)
     loss = sum(np.any(z != t) for z, t in zip(y_pred, y_true))
     if normalize:
-        loss /= float(max(len(y_true), 1))
+        loss /= float(len(y_true))
     return loss
 
 
--- ml1_mnist/metrics/test_metrics.py
+++ ml1_mnist/metrics/test_metrics.py
@@ -1,6 +1,44 @@
 import numpy as np
+from nose.tools import assert_raises
 
-from _metrics import zero_one_loss
+from _metrics import accuracy_score, zero_one_loss
+
+
+class TestAccuracyScore(object):
+
+    def test_equal(self):
+        y_true, y_pred = [1, 2, 3, 4], [1, 2, 3, 4]
+        np.testing.assert_almost_equal(1., accuracy_score(y_true, y_pred))
+        assert accuracy_score(y_true, y_pred, normalize=False) == 4
+
+    def test_not_equal(self):
+        y_true, y_pred = [2, 2, 3, 4], [1, 2, 3, 4]
+        np.testing.assert_almost_equal(0.75, accuracy_score(y_true, y_pred))
+        assert accuracy_score(y_true, y_pred, normalize=False) == 3
+
+    def test_str_equal(self):
+        y_true, y_pred = ['1', '2', '3', '4'], ['1', '2', '3', '4']
+        np.testing.assert_almost_equal(1., accuracy_score(y_true, y_pred))
+        assert accuracy_score(y_true, y_pred, normalize=False) == 4
+
+    def test_str_not_equal(self):
+        y_true, y_pred = ['2', '2', '3', '4'], ['1', '2', '3', '4']
+        np.testing.assert_almost_equal(0.75, accuracy_score(y_true, y_pred))
+        assert accuracy_score(y_true, y_pred, normalize=False) == 3
+
+    def test_multilabel_equal(self):
+        y_true, y_pred = [[0, 1], [0, 1], [1, 0]], [[0, 1], [0, 1], [1, 0]]
+        np.testing.assert_almost_equal(1., accuracy_score(y_true, y_pred))
+        assert accuracy_score(y_true, y_pred, normalize=False) == 3
+
+    def test_multilabel_not_equal(self):
+        y_true, y_pred = np.array([[0, 1], [1, 1], [0, 0]]), np.ones((3, 2))
+        np.testing.assert_almost_equal(1./3., accuracy_score(y_true, y_pred))
+        assert accuracy_score(y_true, y_pred, normalize=False) == 1
+
+    def test_empty(self):
+        y_true, y_pred = [], []
+        assert_raises(Exception, accuracy_score, y_true, y_pred)
 
 
 class TestZeroOneLoss(object):
@@ -37,4 +75,4 @@ class TestZeroOneLoss(object):
 
     def test_empty(self):
         y_true, y_pred = [], []
-        np.testing.assert_almost_equal(0., zero_one_loss(y_true, y_pred))
+        assert_raises(Exception, zero_one_loss, y_true, y_pred)
