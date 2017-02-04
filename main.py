57eaa8e7648a821ae103d64582135405af6f8503
monsta <yell.bondarenko@gmail.com>
2017-02-04 18:08:38 +0100

    added final Logistic Regression

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ml1_mnist.nn import NNClassifier, RBM
 from ml1_mnist.nn.layers import FullyConnected, Activation, Dropout
 from ml1_mnist.nn.activations import leaky_relu
 from ml1_mnist.knn import KNNClassifier
+from ml1_mnist.logreg import LogisticRegression
 from ml1_mnist.decomposition import PCA
 
 from ml1_mnist.utils import (Stopwatch, 
@@ -20,6 +21,12 @@ from ml1_mnist.model_selection import TrainTestSplitter
 
 
 def _train_nn(X, y):
+	"""
+	Parameters
+	----------
+	X : training data
+	y : training labels (not one-hot encoded!)
+	"""
 	print "Running '_train_nn'"
 
 	aug = RandomAugmentator(transform_shape=(28, 28), random_seed=1337)
@@ -220,6 +227,95 @@ def nn(load_nn=True):
 	print "\nTest accuracy {0:.4f} (error {1:.2f}%)".format(acc, 100. * (1. - acc))
 
 
+def logreg(load_logreg=True, load_nn=True):
+	"""
+	Output (if `load_logreg` is False and `load_nn` is True)
+	--------------------------------------------------------
+	Running 'logreg'
+	Loading data ...
+	Loading NN ...
+	Extracting feature vectors ... Elapsed time: 30.346 sec
+	Training LogReg ... Total number of parameters:  1290
+	Train on 49999 samples, validate on 10001 samples
+
+	Epoch   1/750  early stopping after 50 ................................
+	elapsed:  0.70 sec - loss: 0.009 - acc.: 0.9989 - val. loss: 0.008 - val. acc.: 0.9988
+	Epoch   2/750  early stopping after 50 ................................
+	elapsed:  2.13 sec - loss: 0.008 - acc.: 0.9991 - val. loss: 0.009 - val. acc.: 0.9986
+	Epoch   3/750  early stopping after 50 ................................
+	elapsed:  3.67 sec - loss: 0.008 - acc.: 0.9991 - val. loss: 0.009 - val. acc.: 0.9986
+
+	(...)
+
+	elapsed: 129.7 sec - loss: 0.009 - acc.: 0.9992 - val. loss: 0.014 - val. acc.: 0.9979
+	Epoch  90/750  early stopping after 2 ................................
+	elapsed: 131.1 sec - loss: 0.009 - acc.: 0.9992 - val. loss: 0.015 - val. acc.: 0.9977
+	Epoch  91/750  early stopping after 1 ................................
+	elapsed: 132.6 sec - loss: 0.009 - acc.: 0.9994 - val. loss: 0.013 - val. acc.: 0.9985
+	Elapsed time: 133.410 sec
+	Evaluating LogReg ... Elapsed time: 0.173 sec
+
+	Test accuracy 0.9899 (error 1.01%)
+	"""
+	print "Running 'logreg'"
+	print "Loading data ..."
+	X_train, y_train = load_mnist(mode='train', path='data/')
+	X_test, y_test = load_mnist(mode='test', path='data/')
+	X_train /= 255.
+	X_test /= 255.
+	y_test = one_hot(y_test)
+
+	if load_nn:
+		print "Loading NN ..."
+		nn = load_model('models/nn.json')
+	else:
+		nn = _train_nn(X_train.copy(), y_train.copy())
+
+	if load_logreg:
+		print "Loading LogReg ..."
+		logreg = load_model('models/logreg.json')
+		print_inline("Extracting feature vectors ... ")
+		with Stopwatch(verbose=True):
+			nn.forward_pass(X_test)
+			X_test = leaky_relu(nn.layers[13]._last_input)	
+	else:
+		print_inline("Extracting feature vectors ... ")
+		with Stopwatch(verbose=True):
+			nn.forward_pass(X_train)
+			X_train = leaky_relu(nn.layers[13]._last_input)
+			nn.forward_pass(X_test)
+			X_test = leaky_relu(nn.layers[13]._last_input)	
+
+		tts = TrainTestSplitter(shuffle=True, random_seed=1337)
+		train, val = tts.split(y_train, train_ratio=50005./60000., stratify=True)
+		X_val = X_train[val]
+		X_train = X_train[train]
+		y_val = y_train[val]
+		y_train = y_train[train]
+		y_train = one_hot(y_train)
+		y_val = one_hot(y_val)
+
+		logreg = LogisticRegression(n_batches=32,
+									L2=0.000316,
+									random_seed=1337,
+									optimizer_params=dict(
+										learning_rate=0.001,
+										max_epochs=750,
+										plot=False,
+										early_stopping=50,
+									verbose=True))
+		print_inline("Training LogReg ... ")
+		with Stopwatch(verbose=True):		
+			logreg.fit(X_train, y_train, X_val=X_val, y_val=y_val)
+
+	print_inline("Evaluating LogReg ... ")
+	with Stopwatch(verbose=True):
+		y_pred = logreg.predict(X_test)
+		acc = accuracy_score(y_test, y_pred)
+		
+	print "\nTest accuracy {0:.4f} (error {1:.2f}%)".format(acc, 100. * (1. - acc))
+
+
 
 if __name__ == '__main__':
 	# Uncomment what to run:
@@ -227,4 +323,5 @@ if __name__ == '__main__':
 	# knn(load_nn=True)
 	# knn_without_nn()
 	# nn(load_nn=True)
+	# logreg(load_logreg=True, load_nn=True)
 	pass
