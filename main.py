c8344600b787369143bc17c2af7caf5d8b2924eb
monsta <yell.bondarenko@gmail.com>
2017-02-05 14:36:36 +0100

    added final GP model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+from ml1_mnist.gp import GPClassifier
 from ml1_mnist.nn import NNClassifier, RBM
 from ml1_mnist.nn.layers import FullyConnected, Activation, Dropout
 from ml1_mnist.nn.activations import leaky_relu
@@ -26,6 +27,46 @@ def _train_nn(X, y):
 	----------
 	X : training data
 	y : training labels (not one-hot encoded!)
+
+	Output
+	------
+	Running '_train_nn'
+	Applying augmentation ... Elapsed time: 112.495 sec
+	Training NN ...
+	Total number of parameters:  3142008
+	Train on 290000 samples, validate on 10000 samples
+
+	Epoch  1/42 ..........................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	elapsed: 327.3 sec - loss: 0.850 - acc.: 0.8904 - val. loss: 0.386 - val. acc.: 0.8889
+
+	(...)
+
+	elapsed: 15714 sec - loss: 0.013 - acc.: 0.9971 - val. loss: 0.040 - val. acc.: 0.9937
+	Epoch 42/42 ..........................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	......................................................................................
+	elapsed: 16100 sec - loss: 0.013 - acc.: 0.9980 - val. loss: 0.038 - val. acc.: 0.9948
+	Left '_train_nn'
 	"""
 	print "Running '_train_nn'"
 
@@ -70,12 +111,12 @@ def _train_nn(X, y):
 	                  n_batches=1024,
 	                  shuffle=True,
 	                  random_seed=1337,
+	                  save_weights=False,
 	                  optimizer_params=dict(
 	                      max_epochs=42,
 	                      # early_stopping=12,
 	                      verbose=True,
 	                      plot=False,
-	                      save_weights=False,
 	                      # plot_dirpath='tmp/learning_curves{0}/'.format(i),
 	                      learning_rate=5e-5
 	                  ))
@@ -84,6 +125,7 @@ def _train_nn(X, y):
 	nn.fit(X_train, y_train, X_val=X_val, y_val=y_val)
 
 	print "Left '_train_nn'"
+	# nn.save('models/nn.json')
 	return nn
 
 
@@ -95,10 +137,9 @@ def knn(load_nn=True):
 	Running 'knn'
 	Loading data ...
 	Loading NN ...
-	Extracting feature vectors ...
-	Elapsed time: 15.606 sec
-	Building k-d tree ... Elapsed time: 0.216 sec
-	Evaluating k-NN ... Elapsed time: 33.015 sec
+	Extracting feature vectors ... Elapsed time: 11.017 sec
+	Building k-d tree ... Elapsed time: 0.186 sec
+	Evaluating k-NN ... Elapsed time: 28.483 sec
 
 	Test accuracy 0.9887 (error 1.13%)
 	"""
@@ -227,14 +268,14 @@ def nn(load_nn=True):
 	print "\nTest accuracy {0:.4f} (error {1:.2f}%)".format(acc, 100. * (1. - acc))
 
 
-def logreg(load_logreg=True, load_nn=True):
+def logreg(load_nn=True):
 	"""
-	Output (if `load_logreg` is False and `load_nn` is True)
-	--------------------------------------------------------
+	Output (if `load_nn` is True)
+	-----------------------------
 	Running 'logreg'
 	Loading data ...
 	Loading NN ...
-	Extracting feature vectors ... Elapsed time: 30.346 sec
+	Extracting feature vectors ... Elapsed time: 12.054 sec
 	Training LogReg ... Total number of parameters:  1290
 	Train on 49999 samples, validate on 10001 samples
 
@@ -247,13 +288,14 @@ def logreg(load_logreg=True, load_nn=True):
 
 	(...)
 
-	elapsed: 129.7 sec - loss: 0.009 - acc.: 0.9992 - val. loss: 0.014 - val. acc.: 0.9979
+	Epoch  89/750  early stopping after 3 ................................
+	elapsed: 59.28 sec - loss: 0.009 - acc.: 0.9992 - val. loss: 0.014 - val. acc.: 0.9979
 	Epoch  90/750  early stopping after 2 ................................
-	elapsed: 131.1 sec - loss: 0.009 - acc.: 0.9992 - val. loss: 0.015 - val. acc.: 0.9977
+	elapsed: 59.92 sec - loss: 0.009 - acc.: 0.9992 - val. loss: 0.015 - val. acc.: 0.9977
 	Epoch  91/750  early stopping after 1 ................................
-	elapsed: 132.6 sec - loss: 0.009 - acc.: 0.9994 - val. loss: 0.013 - val. acc.: 0.9985
-	Elapsed time: 133.410 sec
-	Evaluating LogReg ... Elapsed time: 0.173 sec
+	elapsed: 60.57 sec - loss: 0.009 - acc.: 0.9994 - val. loss: 0.013 - val. acc.: 0.9985
+	Elapsed time: 60.960 sec
+	Evaluating LogReg ... Elapsed time: 0.057 sec
 
 	Test accuracy 0.9899 (error 1.01%)
 	"""
@@ -271,42 +313,32 @@ def logreg(load_logreg=True, load_nn=True):
 	else:
 		nn = _train_nn(X_train.copy(), y_train.copy())
 
-	if load_logreg:
-		print "Loading LogReg ..."
-		logreg = load_model('models/logreg.json')
-		print_inline("Extracting feature vectors ... ")
-		with Stopwatch(verbose=True):
-			nn.forward_pass(X_test)
-			X_test = leaky_relu(nn.layers[13]._last_input)	
-	else:
-		print_inline("Extracting feature vectors ... ")
-		with Stopwatch(verbose=True):
-			nn.forward_pass(X_train)
-			X_train = leaky_relu(nn.layers[13]._last_input)
-			nn.forward_pass(X_test)
-			X_test = leaky_relu(nn.layers[13]._last_input)	
+	print_inline("Extracting feature vectors ... ")
+	with Stopwatch(verbose=True):
+		nn.forward_pass(X_train)
+		X_train = leaky_relu(nn.layers[13]._last_input)
+		nn.forward_pass(X_test)
+		X_test = leaky_relu(nn.layers[13]._last_input)	
 
-		tts = TrainTestSplitter(shuffle=True, random_seed=1337)
-		train, val = tts.split(y_train, train_ratio=50005./60000., stratify=True)
-		X_val = X_train[val]
-		X_train = X_train[train]
-		y_val = y_train[val]
-		y_train = y_train[train]
-		y_train = one_hot(y_train)
-		y_val = one_hot(y_val)
+	tts = TrainTestSplitter(shuffle=True, random_seed=1337)
+	train, val = tts.split(y_train, train_ratio=50005./60000., stratify=True)
+	X_val = X_train[val]
+	X_train = X_train[train]
+	y_val = one_hot(y_train[val])
+	y_train = one_hot(y_train[train])
 
-		logreg = LogisticRegression(n_batches=32,
-									L2=0.000316,
-									random_seed=1337,
-									optimizer_params=dict(
-										learning_rate=0.001,
-										max_epochs=750,
-										plot=False,
-										early_stopping=50,
-									verbose=True))
-		print_inline("Training LogReg ... ")
-		with Stopwatch(verbose=True):		
-			logreg.fit(X_train, y_train, X_val=X_val, y_val=y_val)
+	logreg = LogisticRegression(n_batches=32,
+								L2=0.000316,
+								random_seed=1337,
+								optimizer_params=dict(
+									learning_rate=0.001,
+									max_epochs=750,
+									plot=False,
+									early_stopping=50,
+								verbose=True))
+	print_inline("Training LogReg ... ")
+	with Stopwatch(verbose=True):		
+		logreg.fit(X_train, y_train, X_val=X_val, y_val=y_val)
 
 	print_inline("Evaluating LogReg ... ")
 	with Stopwatch(verbose=True):
@@ -316,12 +348,76 @@ def logreg(load_logreg=True, load_nn=True):
 	print "\nTest accuracy {0:.4f} (error {1:.2f}%)".format(acc, 100. * (1. - acc))
 
 
+def gp(load_nn=True):
+	"""
+	Output (if `load_nn` is True)
+	-----------------------------
+	Running 'gp'
+	Loading data ...
+	Loading NN ...
+	Extracting feature vectors ... Elapsed time: 1.932 sec
+	Training GP ... Elapsed time: 403.602 sec
+	Evaluating GP ... Elapsed time: 22.021 sec
+
+	Test accuracy 0.9839 (error 1.61%)
+	"""
+	print "Running 'gp'"
+	print "Loading data ..."
+	X_train, y_train = load_mnist(mode='train', path='data/')
+	X_test, y_test = load_mnist(mode='test', path='data/')
+	X_train /= 255.
+	X_test /= 255.
+	y_test = one_hot(y_test)
+
+	if load_nn:
+		print "Loading NN ..."
+		nn = load_model('models/nn.json')
+	else:
+		nn = _train_nn(X_train.copy(), y_train.copy())
+
+	tts = TrainTestSplitter(shuffle=True, random_seed=1337)
+	indices, _ = tts.split(y_train, train_ratio=800./60000., stratify=True) # 794
+	X_train = X_train[indices]
+	y_train = one_hot(y_train[indices])
+
+	print_inline("Extracting feature vectors ... ")
+	with Stopwatch(verbose=True):
+		nn.forward_pass(X_train)
+		X_train = leaky_relu(nn.layers[13]._last_input)
+		nn.forward_pass(X_test)
+		X_test = leaky_relu(nn.layers[13]._last_input)	
+
+	gp = GPClassifier(algorithm='cg', 
+                      kernel='rbf',
+                      kernel_params=dict(
+                      	  sigma=0.4217,
+                      	  gamma=0.0008511
+                      ),
+                      sigma_n=0.,
+                      max_iter=10000, 
+                      tol=1e-7,
+                      cg_tol=1e-7, 
+                      n_samples=2000,
+                      random_seed=1337)
+	print_inline("Training GP ... ")
+	with Stopwatch(verbose=True):	
+		gp.fit(X_train, y_train)
+
+	print_inline("Evaluating GP ... ")
+	with Stopwatch(verbose=True):
+		y_pred = gp.predict(X_test)
+		acc = accuracy_score(y_test, y_pred)
+	
+	print "\nTest accuracy {0:.4f} (error {1:.2f}%)".format(acc, 100. * (1. - acc))
+
+
 
 if __name__ == '__main__':
 	# Uncomment what to run:
 	# ----------------------
 	# knn(load_nn=True)
 	# knn_without_nn()
-	# nn(load_nn=True)
-	# logreg(load_logreg=True, load_nn=True)
+	# nn(load_nn=False)
+	# logreg(load_nn=True)
+	# gp(load_nn=True)
 	pass
