c4b67931c8ffcffaa57316d99090d8e695bf4a51
monsta <yell.bondarenko@gmail.com>
2017-08-19 23:51:24 +0200

    update fetch models script

--- models/fetch_models.sh
+++ models/fetch_models.sh
@@ -1,3 +1,3 @@
-wget "https://www.dropbox.com/s/2x9850zojh6ce35/ml1_mnist_models.zip"
-unzip ml1_mnist_models.zip
-rm -rf ml1_mnist_models.zip
+wget "https://www.dropbox.com/s/2x9850zojh6ce35/ml_mnist_models.zip"
+unzip ml_mnist_models.zip
+rm -rf ml_mnist_models.zip
