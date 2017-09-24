2de3198c34d97420900e9c7b9a583745a4eff8c6
Yelysei Bondarenko <yell.bondarenko@gmail.com>
2017-09-24 20:12:07 +0300

    Update fetch_models.sh

--- models/fetch_models.sh
+++ models/fetch_models.sh
@@ -1,3 +1,4 @@
-wget "https://www.dropbox.com/s/2x9850zojh6ce35/ml_mnist_models.zip"
+#!/usr/bin/env bash
+wget "https://www.dropbox.com/s/5vit0ng4ahwr651/ml_mnist_models.zip"
 unzip ml_mnist_models.zip
 rm -rf ml_mnist_models.zip
